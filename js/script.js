76060b30f44b9e98b1b2086b32334b1fdb823a8b
chvaldez10 <christianvaldez021@gmail.com>
2023-07-14 16:48:30 -0600

    added scroll animation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector("nav").classList.add("nav-background-change");
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 20) {
+    $("nav").addClass("nav-background-change");
   } else {
-    document.querySelector("nav").classList.remove("nav-background-change");
-    document.querySelector("nav").classList.add("nav-background-change-back");
-    console.log("scroll -50");
+    $("nav").removeClass("nav-background-change");
+    $("nav").addClass("nav-background-change-back");
   }
-}
+});
+
+$("#about-me-a").click(function () {
+  console.log("here");
+  $("html, body").animate({ scrollTop: $("#about-me").offset().top }, 1000);
+});
+
+$("#BSC-a").click(function () {
+  console.log("here");
+  $("html, body").animate({ scrollTop: $("#BSC").offset().top }, 1000);
+});
+
+$("#CMG-a").click(function () {
+  console.log("here");
+  $("html, body").animate({ scrollTop: $("#CMG").offset().top }, 1000);
+});
+
+$("#BT-lab-a").click(function () {
+  console.log("here");
+  $("html, body").animate({ scrollTop: $("#BT-lab").offset().top }, 1000);
+});
+
+$("#CFMC-a").click(function () {
+  console.log("here");
+  $("html, body").animate({ scrollTop: $("#CFMC").offset().top }, 1000);
+});
