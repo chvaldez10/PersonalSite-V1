9fe59ea1798de87016351381c47d2f17d745e831
chvaldez10 <christianvaldez021@gmail.com>
2023-08-14 15:29:57 -0600

    cleanup

---
+++ js/circuit-animations.js
@@ -0,0 +1,13 @@
+const latchSwitch = document.getElementById("latchSwitch");
+const bulb = document.querySelector(".bulb");
+const circuitSwitch = document.querySelector(".rotate-45");
+
+latchSwitch.addEventListener("change", function () {
+  if (latchSwitch.checked) {
+    bulb.classList.add("on");
+    circuitSwitch.classList.remove("rotate-45");
+  } else {
+    bulb.classList.remove("on");
+    circuitSwitch.classList.add("rotate-45");
+  }
+});
